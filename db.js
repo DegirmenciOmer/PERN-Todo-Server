d8aa935d198da8366feca697c924f767aaa85894
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-10-01 13:36:45 +0200

    add commit

--- db.js
+++ db.js
@@ -6,6 +6,12 @@ dotenv.config()
 const { Pool } = pg
 
 const pool = new Pool({
+  connectionString:
+    process.env.DATABASE_URL || 'postgres://localhost:5432/perntodo',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   user: 'postgres',
   password: process.env.DB_PASSWORD,
   host: '127.0.0.1',
