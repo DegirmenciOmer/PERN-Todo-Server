5025d540ec3ff468dabe0047c55e95c8f1aaf718
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-10-01 08:50:13 +0200

    commit

--- db.js
+++ db.js
@@ -8,9 +8,19 @@ const { Pool } = pg
 const pool = new Pool({
   user: 'postgres',
   password: process.env.DB_PASSWORD,
-  host: 'localhost',
-  port: 5432,
+  host: '127.0.0.1',
+  port: process.env.port,
   database: 'perntodo',
+})
+
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  user: process.env.PG_USER, //postgres user
+  host: process.env.PG_ENDPOINT, //localhost (I also tried 127.0.0.1)
+  database: process.env.PG_DB, //database name to connect to
+  password: process.env.PG_PASS, //postgres user password
+  port: process.env.PG_PORT, //5432
 })
 
 export default pool
