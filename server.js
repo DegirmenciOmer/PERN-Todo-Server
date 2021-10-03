164772abf8f9e604d9bf5c43501312a2199bd285
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-10-03 20:54:08 +0200

    activate is_done column in the server

--- server.js
+++ server.js
@@ -71,12 +71,26 @@ app.put('/todos/:id', async (req, res) => {
     const { id } = req.params
     const { description } = req.body
 
-    const updateTodo = await pool.query(
-      'UPDATE todo SET description=$1 WHERE todo_id = $2',
-      [description, id]
-    )
+    await pool.query('UPDATE todo SET description=$1 WHERE todo_id = $2', [
+      description,
+      id,
+    ])
 
     res.json('Todo was updated.')
+  } catch (error) {
+    console.error(error.message)
+    res.json(error.message)
+  }
+})
+
+//mark a todo as done
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+
+    await pool.query('UPDATE todo SET is_done = NOT is_done WHERE id = $1', [
+      id,
+    ])
   } catch (error) {
     console.error(error.message)
     res.json(error.message)
@@ -88,9 +102,7 @@ app.delete('/todos/:id', async (req, res) => {
   try {
     const { id } = req.params
 
-    const deleteTodo = await pool.query('DELETE FROM todo WHERE todo_id = $1', [
-      id,
-    ])
+    await pool.query('DELETE FROM todo WHERE todo_id = $1', [id])
 
     res.json('Todo was deleted.')
   } catch (error) {
