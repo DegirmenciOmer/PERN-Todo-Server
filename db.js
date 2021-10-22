da32947f1a59e201551de60f602a5772223d1b62
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-10-22 15:57:32 +0200

    authentication

--- db.js
+++ db.js
@@ -6,16 +6,10 @@ dotenv.config()
 const { Pool } = pg
 
 const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_URL || 'postgres://localhost:5432/perntodo',
-  ssl: {
-    rejectUnauthorized: false,
-  },
-
   user: 'postgres',
   password: process.env.DB_PASSWORD,
-  host: '5432',
-  port: process.env.port,
+  host: 'localhost',
+  port: 5432,
   database: 'perntodo',
 })
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import pool from './db.js'
 import cors from 'cors'
+import asyncHandler from 'express-async-handler'
+import { v4 as uuidV4 } from 'uuid'
 
 const app = express()
 app.use(
@@ -14,34 +16,98 @@ app.use(express.json())
 
 export const PORT = process.env.PORT || 5000
 
-//add new todo
-app.post('/todos', async (req, res) => {
+// register user
+app.post('/users/register', async (req, res) => {
   try {
-    const { description } = req.body
+    const id = uuidV4()
+    console.log({ id })
+    const { name, email, password } = req.body
+    res.setHeader('Content-Type', 'application/json')
+
+    const { rowCount: alreadyExists } = await pool.query(
+      'SELECT * FROM users WHERE email = $1',
+      [email]
+    )
+    console.log({ alreadyExists })
+    if (alreadyExists) {
+      res.sendStatus(400)
+      throw new Error('This email adress already exists')
+    } else {
+      const newUser = await pool.query(
+        'INSERT INTO users(name, email, password, user_id) VALUES($1, $2, $3, $4) RETURNING *',
+        [name, email, password, id]
+      )
+      res.status(201).json(newUser.rows[0])
+    }
+  } catch (error) {
+    console.log(error.message)
+    res.send(error.message)
+  }
+})
+
+//Login user
+app.post('/users/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    res.setHeader('Content-Type', 'application/json')
+
+    console.log({ email, password })
+
+    const user = await pool.query('SELECT * FROM users WHERE email = $1', [
+      email,
+    ])
+    const selectedUser = user.rows[0]
+    console.log({ selectedUser, user })
+    if (!selectedUser) {
+      res.status(404)
+      throw new Error('Please sign up first')
+    } else if (selectedUser && selectedUser.password !== password) {
+      res.status(404)
+      throw new Error('Incorrect password')
+    } else {
+      res.send(selectedUser)
+    }
+  } catch (error) {
+    console.log(error.message)
+    res.send(error.message)
+  }
+})
+
+//add new todo
+app.post('/todos/', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  try {
+    const { description, user_id } = req.body
     if (description.length > 55) {
       res.status(400)
       throw new Error('Too long text')
     }
-    res.setHeader('Content-Type', 'application/json')
-    const newTodo = await pool.query(
-      'INSERT INTO todo (description) VALUES($1) RETURNING *',
-      [description]
-    )
-    console.log('Success')
 
+    console.log(req.body.user_id)
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description, user_id) VALUES($1, $2) RETURNING *',
+      [description, user_id]
+    )
+
+    console.log(newTodo.rows[0])
     res.json(newTodo.rows[0])
   } catch (err) {
     console.error(err.message)
-    res.status(400).json({ message: err.message })
+    res.json(err)
   }
 })
 
 //get all todos
-app.get('/todos', async (req, res) => {
+app.get('/todos/:id', async (req, res) => {
   try {
+    const { id } = req.params
+    console.log({ id })
     const allTodos = await pool.query(
-      'SELECT * FROM todo ORDER BY updated_at DESC'
+      'SELECT * FROM todo WHERE user_id = $1 ORDER BY created_at DESC ',
+      [id]
     )
+
+    console.log(allTodos.rows)
 
     res.json(allTodos.rows)
   } catch (error) {
@@ -51,30 +117,31 @@ app.get('/todos', async (req, res) => {
 })
 
 //get one todo
-app.get('/todos/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const selectedTodo = await pool.query(
-      'SELECT * FROM todo WHERE todo_id = $1',
-      [id]
-    )
+// app.get('/todos/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params
+//     const selectedTodo = await pool.query(
+//       'SELECT * FROM todo WHERE todo_id = $1',
+//       [id]
+//     )
 
-    res.json(selectedTodo.rows[0])
-  } catch (error) {
-    console.error(error.message)
-    res.json(error.message)
-  }
-})
+//     res.json(selectedTodo.rows[0])
+//   } catch (error) {
+//     console.error(error.message)
+//     res.json(error.message)
+//   }
+// })
+
 //update a todo
 app.put('/todos/:id', async (req, res) => {
   try {
     const { id } = req.params
     const { description } = req.body
 
-    const updateTodo = await pool.query(
-      'UPDATE todo SET description=$1 WHERE todo_id = $2',
-      [description, id]
-    )
+    await pool.query('UPDATE todo SET description=$1 WHERE todo_id = $2', [
+      description,
+      id,
+    ])
 
     res.json('Todo was updated.')
   } catch (error) {
