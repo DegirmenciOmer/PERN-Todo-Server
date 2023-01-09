d00c7b59c234845a05be4f8754eebd185ab4177e
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-01-09 23:34:16 +0100

    edit db for deploy

--- db.js
+++ db.js
@@ -1,22 +1,36 @@
-import pg from 'pg'
-import dotenv from 'dotenv'
+import pg from "pg";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const { Pool } = pg
+const { Pool } = pg;
 
 const pool = new Pool({
   connectionString:
-    process.env.DATABASE_URL || 'postgres://localhost:5432/perntodo',
+    process.env.DATABASE_URL || "postgres://localhost:5432/perntodo",
   ssl: {
     rejectUnauthorized: false,
   },
 
-  user: 'postgres',
-  password: process.env.DB_PASSWORD,
-  host: '5432',
-  port: process.env.port,
-  database: 'perntodo',
-})
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+});
 
-export default pool
+// const pool = new Pool({
+//   connectionString:
+//     process.env.DATABASE_URL || 'postgres://localhost:5432/perntodo',
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+
+//   user: 'postgres',
+//   password: process.env.DB_PASSWORD,
+//   host: '5432',
+//   port: process.env.port,
+//   database: 'perntodo',
+// })
+
+export default pool;
